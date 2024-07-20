1fca98cb235cd3bc0f18d031ea8e64930d1e4d21
Cheng Xi Tsou <chengxi600@gmail.com>
2024-07-20 15:50:17 -0400

    show cart items from store and total price

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -3,8 +3,13 @@ import React from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { ItemInfo } from "../data/cartSlice";
 
-export default function CartItem() {
+export default function CartItem({
+  itemInfo
+}: {
+  itemInfo: ItemInfo
+}) {
 
     return (
         <ListItem
@@ -23,8 +28,8 @@ export default function CartItem() {
         }
       >
         <ListItemText
-          primary="Single-line item"
-          secondary="x1"
+          primary={`${itemInfo.name} - $${itemInfo.price} NTD`}
+          secondary={`x${itemInfo.quantity}`}
         />
       </ListItem>
     )
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,23 +1,37 @@
 import { Typography, Grid, List, Paper, Divider, Box } from "@mui/material";
 import CartItem from "../components/CartItem";
+import { useAppSelector } from "../hooks";
+import React, { useMemo } from "react";
 
 export default function CartPage() {
+
+  const items = useAppSelector(state => state.cart.items)
+  const totalPrice = useMemo(() => {
+    let total = 0
+    for(let prop in items) total += (items[prop].price * items[prop].quantity)
+    return total
+  }, [items])
+
+  let cartList = [];
+  for(let prop in items) {
+    cartList.push(
+      <React.Fragment>
+        <CartItem itemInfo={items[prop]} />
+        <Divider/>
+      </React.Fragment>
+    )
+  }
 
     return (
       <Grid container>
         <Grid item xs={6} md={4} margin={3}>
           <Paper elevation={3}>
           <List>
-              <CartItem />
-              <Divider />
-              <CartItem />
-              <Divider />
-              <CartItem />
-              <Divider />
+              {cartList}
           </List>
           <Box display="flex" justifyContent="flex-end" padding={2}>
             <Typography>
-              Total: $10 NTD
+              Total: ${totalPrice} NTD
             </Typography>
           </Box>
           </Paper>
