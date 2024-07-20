fd25c90b6135809a9af6fba8750127062f8614bf
Cheng Xi Tsou <chengxi600@gmail.com>
2024-07-20 15:58:18 -0400

    change cart item quantity functionality

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-import { ItemInfo } from "../data/cartSlice";
+import { addOneToCart, ItemInfo, removeAllFromCart, removeOneFromCart } from "../data/cartSlice";
+import { useAppDispatch } from "../hooks";
 
 export default function CartItem({
   itemInfo
@@ -11,26 +12,28 @@ export default function CartItem({
   itemInfo: ItemInfo
 }) {
 
-    return (
-        <ListItem
-        secondaryAction={
-          <React.Fragment>
-            <IconButton edge="end" aria-label="add">
-              <AddIcon />
-            </IconButton>
-            <IconButton edge="end" aria-label="minus">
-              <RemoveIcon />
-            </IconButton>
-            <IconButton edge="end" aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-          </React.Fragment>
-        }
-      >
-        <ListItemText
-          primary={`${itemInfo.name} - $${itemInfo.price} NTD`}
-          secondary={`x${itemInfo.quantity}`}
-        />
-      </ListItem>
-    )
+  const dispatch = useAppDispatch()
+
+  return (
+      <ListItem
+      secondaryAction={
+        <React.Fragment>
+          <IconButton edge="end" aria-label="add" onClick={() => dispatch(addOneToCart(itemInfo))}>
+            <AddIcon />
+          </IconButton>
+          <IconButton edge="end" aria-label="minus" onClick={() => dispatch(removeOneFromCart(itemInfo))}>
+            <RemoveIcon />
+          </IconButton>
+          <IconButton edge="end" aria-label="delete" onClick={() => dispatch(removeAllFromCart(itemInfo))}>
+            <DeleteIcon />
+          </IconButton>
+        </React.Fragment>
+      }
+    >
+      <ListItemText
+        primary={`${itemInfo.name} - $${itemInfo.price} NTD`}
+        secondary={`x${itemInfo.quantity}`}
+      />
+    </ListItem>
+  )
 }
